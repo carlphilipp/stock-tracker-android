d059751febc6443746fb6d779b3a108b4c98d0b2
Carl-Philipp Harmant <cp.harmant@gmail.com>
2017-09-22 19:05:54 -0500

    More Kotlin convert

---
+++ src/fr/cph/stock/android/activity/BaseActivity.kt
@@ -0,0 +1,132 @@
+/**
+ * Copyright 2013 Carl-Philipp Harmant
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fr.cph.stock.android.activity
+
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.annotation.TargetApi
+import android.app.Activity
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+
+import org.json.JSONObject
+
+import java.util.HashMap
+
+import fr.cph.stock.android.Constants
+import fr.cph.stock.android.R
+import fr.cph.stock.android.domain.Portfolio
+import fr.cph.stock.android.domain.UrlType
+import fr.cph.stock.android.task.MainTask
+
+/**
+ * This class represents the base activity of the app
+ *
+ * @author Carl-Philipp Harmant
+ */
+class BaseActivity : Activity() {
+    private var mLoginStatusView: View? = null
+    private lateinit var login: String
+    private lateinit var password: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.v(TAG, "BaseActivity onCreate")
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.loading)
+        mLoginStatusView = findViewById(R.id.login_status)
+        val settings = getSharedPreferences(Constants.PREFS_NAME, 0)
+
+        if (settings.contains(Constants.LOGIN) && settings.contains(Constants.PASSWORD)) {
+            showProgress(true, null)
+            login = settings.getString(Constants.LOGIN, null)
+            password = settings.getString(Constants.PASSWORD, null)
+            val params = object : HashMap<String, String>() {
+                init {
+                    put("login", login)
+                    put("password", password)
+                }
+            }
+            val main = MainTask(this, UrlType.AUTH, params)
+            main.execute(null as Void?)
+        } else {
+            showProgress(false, null)
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    /**
+     * Show progress bar
+     *
+     * @param show         show the bar or not
+     * @param errorMessage the error message
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private fun showProgress(show: Boolean, errorMessage: String?) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        val shortAnimTime = resources.getInteger(android.R.integer.config_shortAnimTime)
+        mLoginStatusView!!.visibility = View.VISIBLE
+        mLoginStatusView!!.animate().setDuration(shortAnimTime.toLong()).alpha((if (show) 1 else 0).toFloat()).setListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                mLoginStatusView!!.visibility = if (show) View.VISIBLE else View.GONE
+            }
+        })
+    }
+
+    /**
+     * Load home
+     *
+     * @param portfolio the portfolio
+     */
+    fun loadHome(portfolio: Portfolio) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.putExtra(Constants.PORTFOLIO, portfolio)
+        showProgress(false, null)
+        finish()
+        startActivity(intent)
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+    }
+
+    /**
+     * Display error
+     *
+     * @param jsonObject the json object
+     */
+    fun displayError(jsonObject: JSONObject) {
+        val intent = Intent(this, ErrorActivity::class.java)
+        intent.putExtra("data", jsonObject.toString())
+        intent.putExtra(Constants.LOGIN, login)
+        intent.putExtra(Constants.PASSWORD, password)
+        startActivity(intent)
+        finish()
+    }
+
+    companion object {
+
+        private val TAG = "Base"
+    }
+}
---
+++ src/fr/cph/stock/android/activity/ChartActivity.kt
@@ -0,0 +1,218 @@
+/**
+ * Copyright 2013 Carl-Philipp Harmant
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fr.cph.stock.android.activity
+
+import android.annotation.SuppressLint
+import android.app.ActionBar
+import android.app.ActionBar.LayoutParams
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.ActivityInfo
+import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.widget.RelativeLayout
+import android.widget.TextView
+
+import org.apache.commons.io.IOUtils
+import org.json.JSONObject
+
+import java.io.IOException
+import java.io.InputStream
+import java.io.StringWriter
+import java.util.Collections
+
+import fr.cph.stock.android.Constants
+import fr.cph.stock.android.R
+import fr.cph.stock.android.StockTrackerApp
+import fr.cph.stock.android.domain.ChartType
+import fr.cph.stock.android.domain.Portfolio
+import fr.cph.stock.android.domain.UrlType
+import fr.cph.stock.android.task.MainTask
+import fr.cph.stock.android.web.DebugWebChromeClient
+
+/**
+ * This class reprents the chart activity
+ *
+ * @author Carl-Philipp Harmant
+ */
+class ChartActivity : Activity(), IStockTrackerActivity {
+
+    /**
+     * Graphics components
+     */
+    private var menuItem: MenuItem? = null
+    private var errorView: TextView? = null
+    private var chartType: ChartType? = null
+    private var portfolio: Portfolio? = null
+    private var webView: WebView? = null
+    private var actionBar2: ActionBar? = null
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.chart_activity)
+
+        portfolio = intent.getParcelableExtra(Constants.PORTFOLIO)
+        chartType = ChartType.getEnum(intent.getStringExtra("chartType"))
+
+        errorView = findViewById(R.id.errorMessage)
+        actionBar2 = getActionBar()
+        webView = findViewById(R.id.webView)
+        val data = data
+        webView!!.webChromeClient = DebugWebChromeClient()
+        val webSettings = webView!!.settings
+        webSettings.javaScriptEnabled = true
+        // myWebView.setBackgroundColor(0x00000000);
+        // myWebView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        webView!!.loadDataWithBaseURL("file:///android_asset/www/", data, "text/html", "UTF-8", null)
+        webView!!.reload()
+    }
+
+    private val data: String?
+        get() {
+            val metrics = DisplayMetrics()
+            windowManager.defaultDisplay.getMetrics(metrics)
+            var data: String? = null
+            var `is`: InputStream? = null
+            try {
+                val writer = StringWriter()
+                when (chartType) {
+                    ChartType.CAPITALIZATION -> {
+                        `is` = applicationContext.assets.open("www/pie.html")
+                        IOUtils.copy(`is`, writer, "UTF8")
+                        data = writer.toString()
+                        data = data.replace("#DATA#", portfolio!!.chartCapData!!)
+                        data = data.replace("#TITLE#", portfolio!!.chartCapTitle!!)
+                        data = data.replace("#DRAW#", portfolio!!.chartCapDraw!!)
+                        data = data.replace("#COMPANIES#", portfolio!!.chartCapCompanies!!)
+                        data = data.replace("#WIDTH#".toRegex(), ((metrics.widthPixels / metrics.density).toInt() - 30).toString() + "")
+                        data = data.replace("#HEIGHT#".toRegex(), ((metrics.widthPixels / metrics.density).toInt() - 30).toString() + "")
+                        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+                        actionBar!!.title = "Capitalization Chart"
+                        webView!!.isHorizontalScrollBarEnabled = false
+                    }
+                    ChartType.SECTOR -> {
+                        `is` = applicationContext.assets.open("www/pie.html")
+                        IOUtils.copy(`is`, writer, "UTF8")
+                        data = writer.toString()
+                        data = data.replace("#DATA#", portfolio!!.chartSectorData!!)
+                        data = data.replace("#TITLE#", portfolio!!.chartSectorTitle!!)
+                        data = data.replace("#DRAW#", portfolio!!.chartSectorDraw!!)
+                        data = data.replace("#COMPANIES#", portfolio!!.chartSectorCompanies!!)
+                        data = data.replace("#WIDTH#".toRegex(), ((metrics.widthPixels / metrics.density).toInt() - 30).toString() + "")
+                        data = data.replace("#HEIGHT#".toRegex(), ((metrics.widthPixels / metrics.density).toInt() - 30).toString() + "")
+                        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+                        actionBar!!.title = "Sector Chart"
+                        webView!!.isHorizontalScrollBarEnabled = false
+                    }
+                    ChartType.SHARE_VALUE -> {
+                        `is` = applicationContext.assets.open("www/share_value.html")
+                        IOUtils.copy(`is`, writer, "UTF8")
+                        data = writer.toString()
+                        data = data.replace("#DATA#", portfolio!!.chartData!!)
+                        data = data.replace("#DRAW#", portfolio!!.chartDraw!!)
+                        data = data.replace("#COLOR#", portfolio!!.chartColors!!)
+                        data = data.replace("#DATE#", portfolio!!.chartDate!!)
+                        data = data.replace("#WIDTH#".toRegex(), ((metrics.widthPixels / metrics.density).toInt() - 30).toString() + "")
+                        data = data.replace("#HEIGHT#".toRegex(), (metrics.heightPixels.toDouble() / metrics.density.toDouble() / 1.35).toInt().toString() + "")
+                        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+                        actionBar!!.title = "Share Value Chart"
+                    }
+                }
+            } catch (e: IOException) {
+                Log.e(TAG, "", e)
+            } finally {
+                IOUtils.closeQuietly(`is`)
+            }
+            return data
+        }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val mainTask: MainTask
+        when (item.itemId) {
+            R.id.action_logout -> {
+                mainTask = MainTask(this, UrlType.LOGOUT, emptyMap())
+                mainTask.execute(null as Void?)
+                return true
+            }
+            R.id.refresh -> {
+                menuItem = item
+                menuItem!!.setActionView(R.layout.progressbar)
+                menuItem!!.expandActionView()
+                mainTask = MainTask(this, UrlType.RELOAD, emptyMap())
+                mainTask.execute(null as Void?)
+                return true
+            }
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun reloadData(portfolio: Portfolio) {
+        menuItem!!.collapseActionView()
+        menuItem!!.actionView = null
+        val resultIntent = Intent()
+        resultIntent.putExtra(Constants.PORTFOLIO, portfolio)
+        this.portfolio = portfolio
+        val data = data
+        webView!!.loadDataWithBaseURL("file:///android_asset/www/", data, "text/html", "UTF-8", null)
+        setResult(Activity.RESULT_OK, resultIntent)
+        val app = application as StockTrackerApp
+        app.toast()
+    }
+
+    override fun displayError(json: JSONObject) {
+        val sessionError = (application as StockTrackerApp).isSessionError(json)
+        if (sessionError) {
+            (application as StockTrackerApp).loadErrorActivity(this, json)
+        } else {
+            errorView!!.text = json.optString("error")
+            val params = RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
+            params.addRule(RelativeLayout.BELOW, errorView!!.id)
+            val webView = findViewById<WebView>(R.id.webView)
+            webView.layoutParams = params
+            menuItem!!.collapseActionView()
+            menuItem!!.actionView = null
+        }
+    }
+
+    override fun logOut() {
+        (application as StockTrackerApp).logOut(this)
+    }
+
+    companion object {
+
+        private val TAG = "ChartActivity"
+    }
+}
